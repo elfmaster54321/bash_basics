341daf33621f037c6fee0b3facf2e2d26d026154
Yonathan Zuniga <yzuniga@ggc.edu>
2019-01-30 20:05:00 -0500

    Hello, this is definitly not a virusgit status! :)

---
+++ 01-read_input_Number_2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello Yonathan! What is your name?"
+read name
+echo "Welcome, Yonathan Zuniga"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in Yonathan Zuniga'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
