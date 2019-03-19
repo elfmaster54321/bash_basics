27844ba8cff804dd4a8cde13db6bb6ba0463d4b0
Yonathan Zuniga <yzuniga@ggc.edu>
2019-03-18 20:13:07 -0400

    asking width and height to get pixels

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-echo "Enter your first number"
+echo "Enter the width"
 read first
-echo "Enter your second number"
+echo "Enter the height"
 read second
-echo "Enter your third number"
-read third
 
-sum=$(( first+second+third ))
+sum=$(( first * second ))
 
-echo "The sum is $sum"
+echo "The number of pixels is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
